88361038d2ee6fa354c28e87e010be337218f661
saeideh-eslamian <s.eslamian.70@gmail.com>
2024-05-26 16:42:17 +0200

    add login and register user with JWT

--- events/urls.py
+++ events/urls.py
@@ -1,7 +1,17 @@
 from django.urls import path
 from events import views
 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
+
 urlpatterns = [
     path('events/',views.EventListCreateView.as_view(), name='event-list'),
     path('events/<int:pk>/', views.EventRetrieveUpdateDestroyView.as_view(), name='event-detail'),
+    path('register/', views.RegisterUserView.as_view(), name='register'),
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
